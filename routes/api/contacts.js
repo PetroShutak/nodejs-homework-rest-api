4ae405882fa787f258c1016f2b3585ba62bbf843
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-07-31 21:21:27 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,35 +3,25 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'home work done' })
-  req.app.get('io').emit('contacts', 'contacts')
-  next();
+  res.json({ message: 'template message' })
+  console.log('я тут працюю');
 })
 
 router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-  req.app.get('io').emit('contacts', 'contacts')
-  next();
+  console.log('тут я теж працюю');
 })
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
-  req.app.get('io').emit('contacts', 'contacts')
-  next();
-
 })
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-  req.app.get('io').emit('contacts', 'contacts')
-  next();
-
 })
 
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
-  req.app.get('io').emit('contacts', 'contacts')
-  next();
 })
 
 module.exports = router
