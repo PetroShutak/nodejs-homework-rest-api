3f9f70a0295d159a83d349acbee3df0459f5d972
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-08-03 21:44:34 +0300

    added validation email

--- utils/validation/contactsValidation.js
+++ utils/validation/contactsValidation.js
@@ -5,13 +5,13 @@ const phonePattern = /^[0-9 ()+-]+$/;
 
 const schemaCreateContact = Joi.object({
   name: Joi.string().pattern(namePattern).min(3).max(30).required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
   phone: Joi.string().pattern(phonePattern).min(7).max(18).required(),
 }).options({ abortEarly: false });
 
 const schemaUpdateContact = Joi.object({
   name: Joi.string().pattern(namePattern).min(3).max(30).optional(),
-  email: Joi.string().email().optional(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
   phone: Joi.string().pattern(phonePattern).min(7).max(18).optional(),
 })
   .or('name', 'email', 'phone')
