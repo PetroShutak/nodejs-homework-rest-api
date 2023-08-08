c3e6e2bffef64f4ac93c09e4b35c1584f346c7b5
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-08-08 19:15:38 +0300

    added config for render

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://shutak:123456789qwerty@atlascluster.59eynnq.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Server running. Use our API on port: 3000");
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
