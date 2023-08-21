4fe907f6be250071b08eaa0190d01873987b95b5
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-08-21 22:25:54 +0300

    added multer

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const multer  = require('multer')
 
 require("dotenv").config();
 
-const swaggerjsdoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
-const swaggerOptions = require("./middlewares/swagger");
+// const swaggerjsdoc = require("swagger-jsdoc");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerOptions = require("./middlewares/swagger");
 
 const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
@@ -15,6 +16,17 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+const multerConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "tmp");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+app.use(multer({ storage: multerConfig }).single("avatar"));
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
@@ -22,11 +34,11 @@ app.use(express.json());
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerjsdoc(swaggerOptions))
-);
+// app.use(
+//   "/api-docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(swaggerjsdoc(swaggerOptions))
+// );
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
